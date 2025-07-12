7d60ba918315ad5a29642f2581a70d51a21ce538
hiram <hiram@mail.com>
2025-07-12 14:45:31 -0600

    fix: routes and nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PokemonTemplateComponent } from './home/pokemon/pokemon-template/pokemon-template.component';
-import { NgModule } from '@angular/core';
 import { PointOfSaleComponent } from './home/point-of-sale/dashboard/point-of-sale.component';
+import { PosComponent } from './home/point-of-sale/pos.component';
+import { NotFoundError } from 'rxjs';
 
 export const routes: Routes = [
     {
@@ -17,14 +18,24 @@ export const routes: Routes = [
     },
     {
         path: 'sales',
-        component: PointOfSaleComponent,
-        title: 'POS'
+        component: PosComponent,
+        title: 'POS',
+        children: [
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+
+            },
+            {
+                path: 'dashboard',
+                component:  PointOfSaleComponent,
+            }
+            
+        ]
+    },
+    {
+        path: '**',
+        component: NotFoundError
     }
 ];
-
-// @NgModule({
-// imports: [RouterModule.forRoot(routes)],
-// exports: [RouterModule]
-// })
-
-// export class AppRoutingModule{ }
--- src/app/home/point-of-sale/pos.component.ts
+++ src/app/home/point-of-sale/pos.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pos',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './pos.component.html',
   styleUrl: './pos.component.css'
 })
 export class PosComponent {
+  header: boolean = true;
+
+
+  constructor(
+    private router: Router
+  ){
+
+  }
 
 }
